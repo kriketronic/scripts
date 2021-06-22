bad6ffbcd180efdfe609c7162d6ff7e1244920bb
Enrique Barbeito <barbeito.enrique@gmail.com>
2021-06-21 21:52:01 -0300

    add manual instalation of Docker Engine

--- ubuntu-new/post-install.sh
+++ ubuntu-new/post-install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-groupadd docker
-usermod -aG docker kike
-
+#zona horaria
 timedatectl set-timezone America/Argentina/Buenos_Aires
 
+#update apt
 apt-get update
+
+#remove old version
+apt-get remove docker docker-engine docker.io containerd runc
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+#set permissions to kike
+groupadd docker
+usermod -aG docker kike
